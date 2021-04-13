f80a0fd21e8f4d7679be6e821c618384c61331b4
Vogel, Philipp <vogelphilipp@icloud.com>
2021-04-13 18:16:08 +0200

    updated for spring event

--- js/addNewEvent.js
+++ js/addNewEvent.js
@@ -1,18 +1,18 @@
 /* --------------------------
 * UPDATE UPCOMING EVENT DATE
-* -------------------------- 
+* --------------------------
 *
 * IMPORTANT: The date has to be in the Pacific Standard Time (PST) zone
 *
 */
-const 
+const
 // Format: YYYY (2020)
 year = 2021,
-// Format: MM (09) or M (9), both are valid 
+// Format: MM (09) or M (9), both are valid
 month = 04,
 // Format: DD (09) or D (9), both are valid
-day = 20,  
-// Format: 0 - 24 / 5 == 5 AM / 17 == 5 PM 
+day = 20,
+// Format: 0 - 24 / 5 == 5 AM / 17 == 5 PM
 hour = 10,
 // Format: MM (09) or M (9), both are valid
 minute = 00;

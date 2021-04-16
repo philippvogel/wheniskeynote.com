1cc2a3fb30a0dda16df111eb548a77bad72f0ef4
Vogel, Philipp <vogelphilipp@icloud.com>
2021-04-16 19:51:11 +0200

    merged from upstream

--- js/addNewEvent.js
+++ js/addNewEvent.js
@@ -21,8 +21,4 @@ minute = 00;
 * UPDATE UPCOMING EVENT NAME
 * --------------------------
 */
-<<<<<<< HEAD
 const eventName = 'Spring Loaded';
-=======
-const eventName = 'Spring Loaded.';
->>>>>>> d4ee063c2bc83c7adc77c1a417c9ad73df53293b
